6330e020a4f229f6c9b64d764282651b11d6a496
pashioya <john.ashioya@gmail.com>
2023-11-26 11:32:10 +0100

    added copilot and styiling changes

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -7,21 +7,21 @@ return {
     opts = {
       ensure_installed = {
         -- "asm_lsp",                -- Assembly (GAS/NASM, GO)
-        "bashls",                 -- Bash
-        "clangd",                 -- C, C++
-        "cssls",                  -- CSS
-        "eslint",                 -- JavaScript and JSX
-        "html",                   -- HTML
-        "jsonls",                 -- JSON
-        "jdtls",                  -- Java
-        "tsserver",               -- JavaScript, Typescript
-        "lua_ls",                 -- Lua
-        "marksman",               -- Markdown
-        "pyright",                -- Python
-        "sqlls",                  -- SQL
-        "tailwindcss",            -- Tailwind CSS
-        "lemminx",                -- XML
-        "yamlls",                 -- YAML
+        "bashls", -- Bash
+        "clangd", -- C, C++
+        "cssls", -- CSS
+        "eslint", -- JavaScript and JSX
+        "html", -- HTML
+        "jsonls", -- JSON
+        "jdtls", -- Java
+        "tsserver", -- JavaScript, Typescript
+        "lua_ls", -- Lua
+        "marksman", -- Markdown
+        "pyright", -- Python
+        "sqlls", -- SQL
+        "tailwindcss", -- Tailwind CSS
+        "lemminx", -- XML
+        "yamlls", -- YAML
       },
     },
   },
@@ -32,21 +32,21 @@ return {
     -- overrides `require("mason-null-ls").setup(...)`
     opts = {
       ensure_installed = {
-        "prettier",               -- General Code Formatter in: JavaScript, TypeScript, Flow, JSX, JSON, CSS, SCSS, Less, HTML, Vue, Angular, GraphQL, Markdown, YAML
-        "xmlformatter",           --XML
-        "yamlfmt",                -- YAML
-        "sql-formatter",        -- SQL
+        "prettier", -- General Code Formatter in: JavaScript, TypeScript, Flow, JSX, JSON, CSS, SCSS, Less, HTML, Vue, Angular, GraphQL, Markdown, YAML
+        "xmlformatter", --XML
+        "yamlfmt", -- YAML
+        "sql-formatter", -- SQL
         "reorder-python-imports", -- Python
-        "markdown-toc",           -- Markdown
-        "jq",                     -- JSON 
-        "fixjson",                -- JSON
-        "cbfmt",                  -- Markdown: Format codblocks
-        "blue",                   -- Python code formatter.
-        "autopep8",               -- Python code to conform to the PEP 8 style guide
-        "beautysh",               -- A Bash beautifier for the masses.
-        "shellharden",            -- Shell
-        "markdownlint",           -- Markdown
-        "clang-format",           -- Clang
+        "markdown-toc", -- Markdown
+        "jq", -- JSON
+        "fixjson", -- JSON
+        "cbfmt", -- Markdown: Format codblocks
+        "blue", -- Python code formatter.
+        "autopep8", -- Python code to conform to the PEP 8 style guide
+        "beautysh", -- A Bash beautifier for the masses.
+        -- "shellharden", -- Shell
+        "markdownlint", -- Markdown
+        "clang-format", -- Clang
       },
     },
   },
@@ -57,11 +57,11 @@ return {
     opts = {
       ensure_installed = {
         "debugpy", -- Python
-        "python",           -- Python
-        "cppdbg",           -- Cpp
-        "node2",            -- Node
-        "js",               -- JavaScript
-        "javadbg",          -- Java
+        "python", -- Python
+        "cppdbg", -- Cpp
+        "node2", -- Node
+        "js", -- JavaScript
+        "javadbg", -- Java
       },
     },
   },
